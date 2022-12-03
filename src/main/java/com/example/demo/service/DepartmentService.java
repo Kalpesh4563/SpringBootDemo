4989c8c6cc06d9786d89bd2abbf06c9a18cbaf19
Kapesh Vispute <visputekalpesh40@gmail.com>
2022-12-03 23:09:38 +0530

    Crud-Operation

---
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Department;
+
+public interface DepartmentService {
+
+	public Department addDept(Department dept);
+
+	public List<Department> getAllDept();
+
+	public Department getById(Long deptId);
+
+	public String deleteDept(Long deptId);
+
+}
---
+++ src/main/java/com/example/demo/service/DepartmentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.Department;
+import com.example.demo.repository.DepartmentRepo;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+	@Autowired
+	private DepartmentRepo deptRepo;
+	
+	@Override
+	public Department addDept(Department dept) {
+		return deptRepo.save(dept);
+	}
+
+	@Override
+	public List<Department> getAllDept() {
+		return deptRepo.findAll();
+	}
+
+	@Override
+	public Department getById(Long deptId) {
+		return deptRepo.findById(deptId).get();
+	}
+
+	@Override
+	public String deleteDept(Long deptId) {
+		deptRepo.deleteById(deptId);
+		return "Deleted Successfully.....";
+	}
+
+}
