dab0b7ac63d84ee186b03c9af896641c0149c53e
Kalpesh4563 <visputekalpesh40@gmail.com>
2024-06-01 23:48:03 -0700

    PUT & Delete API for Dept created

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -1,17 +1,11 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Department {
 
     @Id
@@ -20,4 +14,45 @@ public class Department {
     String deptName;
     String deptCode;
     String deptAddress;
+
+    public Department() {   }
+
+    public Department(Long deptId, String deptName, String deptCode, String deptAddress) {
+        this.deptId = deptId;
+        this.deptName = deptName;
+        this.deptCode = deptCode;
+        this.deptAddress = deptAddress;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
+    }
+
+    public String getDeptAddress() {
+        return deptAddress;
+    }
+
+    public void setDeptAddress(String deptAddress) {
+        this.deptAddress = deptAddress;
+    }
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Department;
@@ -21,4 +24,36 @@ public class DepartmentServiceImpl implements DepartmentService{
 	public List<Department> getAllDept() {
 		return deptRepo.findAll();
 	}
+
+	@Override
+	public String deleteDept(Long deptId) {
+		deptRepo.deleteById(deptId);
+		return "Department deleted successfully !";
+	}
+
+	@Override
+	public Optional<Department> fetchDeptById(Long deptId) {
+		return deptRepo.findById(deptId);
+	}
+
+	@Override
+	public Department updateDept(Department department, Long deptId) {
+
+		Department deptDb = deptRepo.findById(deptId).get();
+
+		if(Objects.nonNull(department.getDeptName()) && !"".equalsIgnoreCase(department.getDeptName())){
+			deptDb.setDeptName(department.getDeptName());
+		}
+
+		if(Objects.nonNull(department.getDeptAddress()) && !"".equalsIgnoreCase(department.getDeptAddress())){
+			deptDb.setDeptAddress(department.getDeptAddress());
+		}
+
+		if(Objects.nonNull(department.getDeptCode()) && !"".equalsIgnoreCase(department.getDeptCode())){
+			deptDb.setDeptCode(department.getDeptCode());
+		}
+		return deptRepo.save(deptDb);
+	}
+
+
 }
